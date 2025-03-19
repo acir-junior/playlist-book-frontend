76373890810c438fa2ec4b4b0343e548c9456b54
Acir Junior <amjr13@icloud.com>
2025-03-19 18:44:08 -0300

    STYLE: Adjust button change theme

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import { ChangeTheme } from "./helpers/change-theme";
 export function Header() {
     return (
         <header>
-            <div className="container mx-auto py-2 px-2 flex items-center justify-end">
+            <div className="container mx-auto px-4 relative top-[5vh] flex items-center justify-end">
                 <ChangeTheme />
             </div>
         </header>
